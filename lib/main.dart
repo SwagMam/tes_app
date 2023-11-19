b4f20bf9f099e6c3d3deb831410c66794d15df83
SwagMam <bagusimamprakoso58@gmail.com>
2023-11-20 00:02:35 +0700

    commit statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,60 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello, World!'),
+          title: Text('Hello, Semuanya ini App Bar'),
         ),
         body: Center(
-          child: Text('Hello, world!'),
+          child: BiggerText(text: 'Hello, world!'),
         ),
       ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textsize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textsize)),
+        ElevatedButton(
+          child: const Text("perbesar"),
+          onPressed: () {
+            setState(() {
+              _textsize = 32.0;
+            });
+          },
+        )
+      ],
     );
   }
 }
